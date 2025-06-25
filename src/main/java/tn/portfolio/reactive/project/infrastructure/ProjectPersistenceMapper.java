99e059a625484fb6ed2f4e05db136a0888f24161
tapioNiemela80 <tapio_niemela1@yahoo.com>
2025-06-25 15:27:03 +0100

    minor fixes

--- src/main/java/tn/portfolio/reactive/project/infrastructure/ProjectPersistenceMapper.java
+++ src/main/java/tn/portfolio/reactive/project/infrastructure/ProjectPersistenceMapper.java
@@ -2,24 +2,22 @@ package tn.portfolio.reactive.project.infrastructure;
 
 import java.util.List;
 
-public final class ProjectPersistenceMapper {
-    public static ProjectDto fromEntity(ProjectEntity entity, List<ProjectTaskEntity> taskEntities) {
+final class ProjectPersistenceMapper {
+    static ProjectDto fromEntity(ProjectEntity entity, List<ProjectTaskEntity> taskEntities) {
         return entity.toDto(taskEntities);
     }
 
-    public static ProjectEntity toEntity(ProjectDto dto, boolean isNew) {
-        ProjectEntity entity = new ProjectEntity(dto.id(),
+    static ProjectEntity toEntity(ProjectDto dto, boolean isNew) {
+        return new ProjectEntity(dto.id(),
                 dto.name(), dto.description(), dto.createdAt(),
                 dto.plannedEndDate(), dto.status(), dto.version(),
                 dto.initialEstimatedTimeHours(), dto.initialEstimatedTimeMinutes(),
                 isNew,
                 dto.contactPersonName(), dto.contactPersonEmail());
-        return entity;
     }
 
-    public static ProjectTaskEntity toEntity(ProjectTaskDto dto) {
-        ProjectTaskEntity entity = new ProjectTaskEntity(dto.id(), dto.title(), dto.description(), dto.estimatedTimeHours(),
+    static ProjectTaskEntity toEntity(ProjectTaskDto dto) {
+        return new ProjectTaskEntity(dto.id(), dto.title(), dto.description(), dto.estimatedTimeHours(),
                 dto.estimatedTimeMinutes(), dto.taskStatus(), dto.projectId(), dto.actualTimeSpentHours(), dto.actualTimeSpentMinutes());
-        return entity;
     }
 }
